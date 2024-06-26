22949a570bcdad26a6e22a7a65ec3590b5fe6fe6
Zyonasec <ezra005@proton.me>
2024-06-27 01:57:56 +0300

    amnetiy

--- models/amenity.py
+++ models/amenity.py
@@ -3,21 +3,17 @@
     Implementation of the Amenity class
 '''
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
-import os
 
 
 class Amenity(BaseModel, Base):
     '''
         Implementation for the Amenities.
     '''
-    __tablename__ = "amenities"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place",
-                                       secondary=place_amenity,
-                                       back_populates="amenities")
     else:
         name = ""
